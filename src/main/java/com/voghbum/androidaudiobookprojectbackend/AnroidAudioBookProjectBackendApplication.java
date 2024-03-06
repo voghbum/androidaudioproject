3b2ef286fa65acaad6b1ed642b498d324dec13a3
huseyin.asci <huseyinascci@gmail.com>
2024-03-06 20:21:09 +0300

    initial commit

---
+++ src/main/java/com/voghbum/androidaudiobookprojectbackend/AnroidAudioBookProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.voghbum.androidaudiobookprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnroidAudioBookProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnroidAudioBookProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/voghbum/androidaudiobookprojectbackend/AnroidAudioBookProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voghbum.androidaudiobookprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnroidAudioBookProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
