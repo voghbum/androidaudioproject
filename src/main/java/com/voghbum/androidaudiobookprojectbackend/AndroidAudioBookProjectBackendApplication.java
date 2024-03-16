f5a465290bded50003984a869fda0855868b3e89
huseyin.asci <huseyinascci@gmail.com>
2024-03-16 14:31:19 +0300

    docker integration

--- src/main/java/com/voghbum/androidaudiobookprojectbackend/AndroidAudioBookProjectBackendApplication.java
+++ src/main/java/com/voghbum/androidaudiobookprojectbackend/AndroidAudioBookProjectBackendApplication.java
@@ -2,9 +2,6 @@ package com.voghbum.androidaudiobookprojectbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 public class AndroidAudioBookProjectBackendApplication {
